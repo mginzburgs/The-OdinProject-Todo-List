90beff0a07d69a8a7ea2637ceb317aa55694411c
MG <mginzburgs@me.com>
2024-09-17 03:25:28 +0300

    task and project logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log(huj);
+import printPage from "./modules/dom";
+import createProject from "./modules/project";
+
+const app = () => {
+  const projects = [];
+
+  return {
+    getProjects() {
+      return projects;
+    },
+
+    getProject(id) {
+      let project = projects.find((e) => e.getId() == id);
+      return project;
+    },
+
+    getProjectByName(projectName) {
+      let project = projects.find(
+        (e) => e.project.toLowerCase() == projectName.toLowerCase()
+      );
+      return project;
+    },
+
+    addProject(project) {
+      projects.push(createProject(project));
+    },
+  };
+};
+
+const TaskIt = app();
+
+TaskIt.addProject("Test");
+TaskIt.addProject("Test2");
+TaskIt.getProjectByName("test2").addTask("buy milk", "tomorrow", "high");
+TaskIt.getProjectByName("test2").getTasks();
+
+console.log(TaskIt.getProjects());
